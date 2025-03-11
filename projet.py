e7d0e36938e506e4851a547e2a3b38fd077bbd2f
Kousay Nasrallah <nasrallahkoussay9@gmail.com>
2025-03-11 09:50:25 +0100

    commiter

--- projet.py
+++ projet.py
@@ -6,6 +6,15 @@ S += t
 return S
 =======
 Data=[1,3,5]
+<<<<<<< HEAD
 Som = sum(Data)
 print("La somme est :",Som)
+>>>>>>> branch1
+=======
+if Data:
+print("La somme est :",Sum(data))
+print("Le min est:",min(Data))
+print("Le max est:",max(Data))
+else:
+print("Liste vide")
 >>>>>>> branch1
